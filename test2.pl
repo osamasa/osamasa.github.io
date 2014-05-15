b4a3010ab62e52cbc3489c1019a014de530375a9
osamasa <osamasa@gmail.com>
2014-05-15 21:48:53 +0900

    ver 1.2.1

--- test2.pl
+++
@@ -1,4 +0,0 @@
-for (my $i =0; $i < 30;$i++) {
-    my $tmp = '<li id="li' . ($i+1) . '" class="cl' . ($i+1) . '"><div class="ui-grid-a"><div class="ui-block-a" style="width:10%;"><h3>' . ($i+1). '</h3></div><div class="ui-block-b" style="width:90%;"><input type="text" /></div></div></li>';
-    print $tmp , "\n";
-}
